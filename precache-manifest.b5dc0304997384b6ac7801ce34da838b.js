f320d970d190291c4df62f254c49ce1c937399ef
Milosm991 <milosmanojlovic91@gmail.com>
2020-07-09 18:38:16 +0200

    Updates

---
+++ precache-manifest.b5dc0304997384b6ac7801ce34da838b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cdd20b6a24c76a08510bb43fb62495c",
+    "url": "/bit-blog/index.html"
+  },
+  {
+    "revision": "70d8d22c8b770943f4e1",
+    "url": "/bit-blog/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "a9cdaeef4770ed416b05",
+    "url": "/bit-blog/static/css/main.29a2ca49.chunk.css"
+  },
+  {
+    "revision": "70d8d22c8b770943f4e1",
+    "url": "/bit-blog/static/js/2.50032d02.chunk.js"
+  },
+  {
+    "revision": "35937c55f4ccbb69aaefc08b51c916a0",
+    "url": "/bit-blog/static/js/2.50032d02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9cdaeef4770ed416b05",
+    "url": "/bit-blog/static/js/main.5eb76974.chunk.js"
+  },
+  {
+    "revision": "c40a2b2c6a89ed0ff0e5",
+    "url": "/bit-blog/static/js/runtime-main.88b66aea.js"
+  }
+]);
